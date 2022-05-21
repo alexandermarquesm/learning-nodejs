6a2233fe494bb5b6797c7fbe9f1a3727324b4698
alexandermarquesm <alexandermarquesoficial@gmail.com>
2022-05-21 17:23:34 -0300

    feat(app.js): criação da rota get usando express

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const http = require('http');
+const exprss = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = exprss();
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    return res.send('Hello World in express');
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => console.log('Running on port 3000'));
